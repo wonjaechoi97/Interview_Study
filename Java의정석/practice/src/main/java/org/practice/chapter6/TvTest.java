19cd8946f67aa9bf10282ff16d779962c111f326
wj529 <wj5295@naver.com>
2023-07-25 11:37:08 +0900

    TvTest1, 한글 깨짐 해결

---
+++ Java의정석/practice/src/main/java/org/practice/chapter6/TvTest.java
@@ -0,0 +1,38 @@
+package org.practice.chapter6;
+
+public class TvTest {
+    public static void main(String[] args) {
+        Tv t = new Tv();
+        t.channel = 7;
+        t.channelDown();
+        System.out.println(" 현재 채널은 "+t.channel+" 입니다.");
+
+    }
+}
+
+class Tv {
+    // TV의 속성(멤버 변수)
+    String color;
+    boolean power;
+    int channel;
+
+    //Tv의 기능(멤버 메서드)
+    void power() {
+        power = !power;
+    }
+
+    void channelUP() {
+        ++channel;
+    }
+
+    void channelDown()
+
+    {
+        --channel;
+    }
+}
+
+
+
+
+
